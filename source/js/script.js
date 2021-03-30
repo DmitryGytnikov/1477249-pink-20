b1337638310ee3b188aea174bae13ee49bbc8710
DmitryGytnikov <gytnikovd@mail.ru>
2021-03-30 17:18:03 +0300

    Добавляет адаптивное меню бургер на Index.html

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log(`obj1`);
+// console.log(`obj1`);
+let burger = document.querySelector(`.header__burger`);
+let headerMenu = document.querySelector(`.header__menu`);
+let header = document.querySelector(`.header`);
+let body = document.querySelector(`.page__body`);
+
+burger.addEventListener(`click`, function (evt) {
+  // evt.preventDefault();
+  burger.classList.toggle(`active`);
+  headerMenu.classList.toggle(`active`);
+  header.classList.toggle(`active`);
+  body.classList.toggle(`lock`);
+});
